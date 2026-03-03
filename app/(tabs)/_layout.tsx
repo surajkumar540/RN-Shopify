90952eba5703160ac1c8f075d2ca84b5c00101ac
Suraj Kumar <surajgautam56878@gmail.com>
2026-03-03 11:17:25 +0530

    add:footer added with home, cart, faviourtes and profile icons with pages

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,34 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
 
 export default function TabLayout() {
   return (
-    <View>
-      <Text>TabLayout</Text>
-    </View>
+    <Tabs>
+      <Tabs.Screen name="index" options={{
+        tabBarIcon: ({
+          color, focused
+        }) => <Ionicons name={focused ? 'home' : "home-outline"} size={26} color={color} />
+      }} />
+
+      <Tabs.Screen name="cart" options={{
+        tabBarIcon: ({
+          color, focused
+        }) => <Ionicons name={focused ? 'cart' : "cart-outline"} size={26} color={color} />
+      }} />
+
+      <Tabs.Screen name="favorites" options={{
+        tabBarIcon: ({
+          color, focused
+        }) => <Ionicons name={focused ? 'heart' : "heart-outline"} size={26} color={color} />
+      }} />
+
+      <Tabs.Screen name="profile" options={{
+        tabBarIcon: ({
+          color, focused
+        }) => <Ionicons name={focused ? 'person' : "person-outline"} size={26} color={color} />
+      }} />
+
+    </Tabs>
   )
 }
