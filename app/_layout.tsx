5a56274e3a6d80f3d0976ca1b9faec7c9d41e871
Suraj Kumar <surajgautam56878@gmail.com>
2026-03-08 12:03:35 +0530

    add:signi and signup and clerk setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,17 +4,29 @@ import { CartProvider } from "@/context/CartContext";
 import { WishListProvider } from "@/context/WishListContext";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Toast from "react-native-toast-message";
+import { ClerkProvider } from "@clerk/clerk-expo";
+
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+
 export default function RootLayout() {
+
+
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
   return (
+
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <CartProvider>
-        <WishListProvider>
-          <Stack screenOptions={{
-            headerShown: false
-          }} />
-          <Toast />
-        </WishListProvider>
-      </CartProvider>
+      <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+        <CartProvider>
+          <WishListProvider>
+            <Stack screenOptions={{
+              headerShown: false
+            }} />
+            <Toast />
+          </WishListProvider>
+        </CartProvider>
+      </ClerkProvider>
+
     </GestureHandlerRootView>
   )
 }
