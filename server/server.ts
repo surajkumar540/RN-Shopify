e3bcc17f4b02862b6bcc9fccdbdc67939c6b5cb4
Suraj Kumar <surajgautam56878@gmail.com>
2026-03-10 15:31:08 +0530

    clerk webhook endpoints created

--- server/server.ts
+++ server/server.ts
@@ -2,12 +2,16 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import { clerkMiddleware } from '@clerk/express'
+import { clerkWebhook } from "./controllers/webhooks.js";
 
 const app = express();
 const PORT = 5000;
 
 
 await connectDB();
+
+app.post("api/clerk", express.raw({ type: 'application/json' }), clerkWebhook);
+
 app.use(express.json());
 app.use(clerkMiddleware());
 
